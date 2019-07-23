5e3cf1362e36fa6a6b08ca30eac4dc930c3409cd
RenMitsumata <vqyw8ade4rrkjh8y4zjb@docomo.ne.jp>
2019-07-23 13:46:00 +0900

    しねえええええええええええええええええええええ

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+   // [SerializeField]
+   //private GameObject gameObject;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("start");
+        //gameObject.GetComponent<GameObject>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("pass");
+        if (Input.GetButtonDown("A"))
+        {
+            Debug.Log("Aボタンが押されました");
+        }
     }
 
 
